dde387076b4afafe0e7e77bd6e1438f6217a464b
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-03-20 19:28:18 +0100

    Ajout des éléments composant le `header` dans le html. Script js pour l'ouverture et la fermeture du menu de navigation pour les appareils mobile.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,37 @@
 /*=============== SHOW MENU ===============*/
+// Constantes des éléments du menu de navigation. 
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/*===== MENU SHOW =====*/
+// Valide l'existence de la constante
+if(navToggle){
+    // Ajoute la classe 'show-menu' à 'navMenu' lorsqu'on clique sur 'navToggle'.
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+// Valide l'existence de la constante
+if(navClose){
+    // Supprime la classe 'show-menu' de 'navMenu' lorsqu'on clique sur 'navClose'.
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+// Constantes qui récupèrent tous les éléments de la classe 'nav__link'.
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // Supprime la classe 'show-menu' de 'navMenu' lorsqu'on clique sur un lien de navigation.
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SHADOW HEADER ===============*/
 
